f42988715279868bb7692464afe8bf20a25af57c
Sahul Kumar <149247512+sahul-git@users.noreply.github.com>
2026-04-16 19:14:13 +0530

    Time: 2 ms (78.35%) | Memory: 7.7 MB (64.74%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -11,10 +11,11 @@ public:
     //     return fibnum(n, dp);
     // }
     int fib(int n) {
+        if(n <= 1) return n;
         int prev1 = 1;
         int prev2 = 0;
         int curr = 0;
-        if(n == 0) return prev2;
+        
         for(int i=2; i<=n; i++){
             curr = prev1 + prev2;
             prev2 = prev1;
