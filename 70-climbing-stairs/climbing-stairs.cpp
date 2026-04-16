500110f95190b0741f5f8baaaccf3963c0fcb51a
Sahul Kumar <149247512+sahul-git@users.noreply.github.com>
2026-04-16 19:21:21 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (98.50%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n<= 2) return n;
+        int prev = 2;
+        int prev2 = 1;
+        for(int i=3; i<=n; i++){
+            int curri = prev + prev2;
+            prev2 = prev;
+            prev = curri;
+        }
+        return prev;
+    }
+};
