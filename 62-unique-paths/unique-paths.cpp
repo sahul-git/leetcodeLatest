eaef0145305015f30de1c6638a6b695652c71b54
Sahul Kumar <149247512+sahul-git@users.noreply.github.com>
2026-04-18 18:39:55 +0530

    Time: 1 ms (25.28%) | Memory: 9.3 MB (50.17%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int sumPath(int i, int j, vector<vector<int>>& dp){
+        if( i==0 && j==0){
+            return 1;
+        }
+        if(i<0 || j<0){
+            return 0;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        
+        return dp[i][j] = sumPath(i-1, j, dp) + sumPath(i, j-1, dp);
+        
+    }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m ,vector<int>(n, -1));
+        return sumPath(m-1, n-1, dp);
+    }
+};
