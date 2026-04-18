3c3ebef11e04896d2d36980ca9f8e1a6f06c399f
Sahul Kumar <149247512+sahul-git@users.noreply.github.com>
2026-04-18 18:47:26 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (28.91%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -2,18 +2,14 @@ class Solution {
 public:
     int sumPath(int i, int j,vector<vector<int>>& obstacleGrid, vector<vector<int>>& dp){
         
-        if( i==0 && j==0){
-            
-            return (obstacleGrid[i][j] == 1) ? 0 : 1;
-        }
-        if(i<0 || j<0){
-            return 0;
-        }
+        if (i < 0 || j < 0) return 0;
+        if(obstacleGrid[i][j]==1) return 0;
+        if (i == 0 && j == 0) return 1; 
         
         if(dp[i][j] != -1){
             return dp[i][j];
         }
-        if(obstacleGrid[i][j] == 1) return 0;
+        
         
         return dp[i][j] = sumPath(i-1, j,obstacleGrid, dp) + sumPath(i, j-1,obstacleGrid, dp);
         
