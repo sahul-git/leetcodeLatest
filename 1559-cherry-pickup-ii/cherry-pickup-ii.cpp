44c0d9fa177ab6194c064c1c3e328d4c52b45655
Sahul Kumar <149247512+sahul-git@users.noreply.github.com>
2026-04-21 16:34:26 +0530

    Time: 84 ms (10.45%) | Memory: 21 MB (52.79%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
@@ -1,37 +1,77 @@
 class Solution {
 public:
-    int maxPickup(int i, int j1, int j2, vector<vector<int>>& grid, vector<vector<vector<int>>>& dp){
-        if(j1 < 0 || j1>= grid[0].size() || j2 < 0 || j2>= grid[0].size()){
-            return -1e9;
-        }
-        if(i>= grid.size()-1){
-            if(j1 == j2){
-                return grid[i][j1];
-            }else{
-                return grid[i][j1] + grid[i][j2];
-            }
-        }
-        if(dp[i][j1][j2] != -1){
-            return dp[i][j1][j2];
-        }
+    // int maxPickup(int i, int j1, int j2, vector<vector<int>>& grid, vector<vector<vector<int>>>& dp){
+    //     if(j1 < 0 || j1>= grid[0].size() || j2 < 0 || j2>= grid[0].size()){
+    //         return -1e9;
+    //     }
+    //     if(i>= grid.size()-1){
+    //         if(j1 == j2){
+    //             return grid[i][j1];
+    //         }else{
+    //             return grid[i][j1] + grid[i][j2];
+    //         }
+    //     }
+    //     if(dp[i][j1][j2] != -1){
+    //         return dp[i][j1][j2];
+    //     }
 
-        int maxi = 0;;
+    //     int maxi = 0;;
         
-        for(int dj1=-1 ; dj1<=1; dj1++){
-            for(int dj2=-1 ; dj2<=1; dj2++){
-                if(j1 == j2){
-                    maxi = max(maxi,grid[i][j1] + maxPickup(i+1, j1+dj1, j2+dj2, grid, dp) ) ;
-                }else{
-                    maxi = max(maxi,grid[i][j1] +grid[i][j2] + maxPickup(i+1, j1+dj1, j2+dj2, grid, dp) ) ;
-                }
-            }
-        }
-        return dp[i][j1][j2] = maxi;
-    }
+    //     for(int dj1=-1 ; dj1<=1; dj1++){
+    //         for(int dj2=-1 ; dj2<=1; dj2++){
+    //             if(j1 == j2){
+    //                 maxi = max(maxi,grid[i][j1] + maxPickup(i+1, j1+dj1, j2+dj2, grid, dp) ) ;
+    //             }else{
+    //                 maxi = max(maxi,grid[i][j1] +grid[i][j2] + maxPickup(i+1, j1+dj1, j2+dj2, grid, dp) ) ;
+    //             }
+    //         }
+    //     }
+    //     return dp[i][j1][j2] = maxi;
+    // }
     int cherryPickup(vector<vector<int>>& grid) {
         int n = grid.size(); 
         int m =grid[0].size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(m, -1)));
-        return maxPickup(0, 0, m-1, grid, dp);
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(m, 0)));
+        for(int j1=0; j1<m; j1++){
+            for(int j2=0; j2<m; j2++){
+                if(j1 == j2){
+                    dp[n-1][j1][j2] = grid[n-1][j1];
+                }else{
+                    dp[n-1][j1][j2] = grid[n-1][j1] + grid[n-1][j2];
+                }
+            }
+        }
+
+        for(int i=n-2; i>=0; i--){
+            for(int j1=0; j1<m; j1++){
+                for(int j2=0; j2<m; j2++){
+                    int maxi = 0;
+                    for(int dj1=-1 ; dj1<=1; dj1++){
+                        for(int dj2=-1 ; dj2<=1; dj2++){
+                            int nj1 = j1 + dj1;
+                            int nj2 = j2 + dj2;
+
+                            if(nj1 >= 0 && nj1 < m && nj2 >= 0 && nj2 < m){
+
+                                int value;
+                                if(j1 == j2)
+                                    value = grid[i][j1];
+                                else
+                                    value = grid[i][j1] + grid[i][j2];
+
+                                value += dp[i+1][nj1][nj2];
+
+                                maxi = max(maxi, value);
+                            }
+
+                            
+                        }
+                    }
+                    dp[i][j1][j2] = maxi;
+                }
+            }
+        }
+        return dp[0][0][m-1];
+        
     }
 };
