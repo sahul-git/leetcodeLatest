1c155d94bd5956410218cbe55f4c33380ac7cbb8
Sahul Kumar <149247512+sahul-git@users.noreply.github.com>
2026-07-04 18:48:04 +0530

    Time: 43 ms (86.57%) | Memory: 24.4 MB (77.11%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -1,45 +1,42 @@
 class Solution {
 public:
     int minimumEffortPath(vector<vector<int>>& heights) {
-        priority_queue<pair<int, pair<int,int>>, 
-                       vector<pair<int, pair<int,int>>>, 
-                       greater<pair<int, pair<int,int>>>> pq;
-        int n = heights.size();
+        int n = heights.size(); 
         int m = heights[0].size();
 
-        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
-        dist[0][0] = 0;
-        pq.push({0,{0,0}});
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
+
+        vector<vector<int>> dist(n, vector<int>(m, 1e9));
         int dr[] = {-1, 0, 1, 0};
-        int dc[] = {0, -1, 0, 1};
+        int dc[] = {0, 1, 0, -1};
+
+        pq.push({0,{0, 0}});
+        dist[0][0] = 0;
+
         while(!pq.empty()){
             auto it = pq.top();
             pq.pop();
+
             int diff = it.first;
             int row = it.second.first;
             int col = it.second.second;
 
             if(row == n-1 && col == m-1) return diff;
-            for(int d = 0; d<4; d++){
-                int nrow = row + dr[d];
-                int ncol = col + dc[d];
-                if(nrow >=0 && ncol>=0 && nrow < n && ncol < m){
-                    int newEffort = max(abs(heights[row][col] - heights[nrow][ncol]), diff);
+
+            for(int i=0; i<4; i++){
+                int nrow = row + dr[i];
+                int ncol = col + dc[i];
+
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m){
+                    int newEffort = max(abs(heights[nrow][ncol] - heights[row][col]), diff);
                     if(newEffort < dist[nrow][ncol]){
                         dist[nrow][ncol] = newEffort;
-                        pq.push({dist[nrow][ncol], {nrow, ncol}});
+                        pq.push({newEffort, {nrow, ncol}});
                     }
-
                 }
             }
-            
-
-
-
-
         }
         return 0;
-
 
     }
 };
