d4b9332c1a30abf0cb8bb32dc82cac4914bccb8e
Sahul Kumar <149247512+sahul-git@users.noreply.github.com>
2026-08-27 15:07:21 +0530

    Time: 24 ms (25.54%) | Memory: 29.8 MB (18.12%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -3,12 +3,29 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         int n = strs.size();
         unordered_map<string, vector<string>> mp;
-        for(int i=0; i<n; i++){
-            string s = strs[i];
-            string key = s;
-            sort(key.begin(), key.end());
+        // for(int i=0; i<n; i++){
+        //     string s = strs[i];
+        //     string key = s;
+        //     sort(key.begin(), key.end());
+        //     mp[key].push_back(s);
+        // }
+        for(string s : strs){
+            vector<int> alpha(26, 0);
+            for(char ch : s){
+                alpha[ch-'a']++;
+            }
+            string key;
+            for(int i=0; i<26; i++){
+                
+                while(alpha[i] > 0){
+                    key.push_back('a'+ i);
+                    alpha[i]--;
+                }
+                
+            }
             mp[key].push_back(s);
         }
+
         vector<vector<string>> ans;
         for(auto ele : mp){
             ans.push_back(ele.second);
