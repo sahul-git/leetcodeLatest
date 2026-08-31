a5a9bea273212437576973ef0d6da4119f013236
Sahul Kumar <149247512+sahul-git@users.noreply.github.com>
2026-08-31 23:37:17 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (65.17%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
+    int maxMoney(int ind, vector<int>&nums, vector<int>& dp){
+        if(ind < 0){
+            return 0;
+        }
+        if(dp[ind] != -1) return dp[ind];
+        int take = nums[ind] + maxMoney(ind-2,nums, dp);
+        int notTake = maxMoney(ind-1, nums, dp);
+
+        return dp[ind] = max(take, notTake);
+    }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        if(n == 1) return nums[0];
-        int prev1 =max(nums[0], nums[1]);
-        int prev2 = nums[0];
-        for(int i = 2; i < n; i++){
-            int curr = max(nums[i] + prev2, prev1);
-            prev2 = prev1;
-            prev1 = curr;
-        }
-
-    return prev1;
+        vector<int> dp(n, -1);
+        return maxMoney(n-1, nums, dp);
     }
 };
